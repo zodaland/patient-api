410735ca69f8e3995cf8219f69ecd13e8f144b7e
zodaland <admin@zodaland.com>
2023-02-08 10:22:05 +0900

    exception 핸들링 및 에러 코드 추가

--- src/main/java/com/patientapi/app/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/patientapi/app/global/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.patientapi.app.global.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.patientapi.app.global.response.Response;
@@ -14,14 +17,27 @@ import com.patientapi.app.global.response.Response;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(ImageProcessException.class)
-	public ResponseEntity<Response> ResponseStatusException(ResponseStatusException e) {
-		e.printStackTrace();
+	public ResponseEntity<Response> imageProcessException(ResponseStatusException e) {
 		return ResponseEntity.internalServerError().body(new Response("1004"));
+	}
+	
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<Response> methodArgumentTypeMismatchException(Exception e) {
+		return ResponseEntity.internalServerError().body(new Response("9996"));
+	}
+	
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<Response> httpRequestMethodNotSupportedException(Exception e) {
+		return ResponseEntity.internalServerError().body(new Response("9997"));
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<Response> httpMessageNotReadableException(Exception e) {
+		return ResponseEntity.internalServerError().body(new Response("9998"));
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Response> Exception(Exception e) {
-		e.printStackTrace();
+	public ResponseEntity<Response> exception(Exception e) {
 		return ResponseEntity.internalServerError().body(new Response("9999"));
 	}
 }
